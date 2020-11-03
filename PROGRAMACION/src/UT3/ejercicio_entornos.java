0caa3190e04233ecef5fc23ed7cf412b63ac59ea
konrad <konradrub14@gmail.com>
2020-11-03 18:53:10 +0100

    ejercicio de entornos

---
+++ PROGRAMACION/src/UT3/ejercicio_entornos.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package UT3;
+import java.util.Scanner; 
+
+/**
+ * @author konrad
+ *
+ */
+public class ejercicio_entornos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//EJERCICIO DE PROGRAMACÓN, SUBO EL ARCHIVO A GITHUB
+		
+
+	}
+
+}
