850e8f6c1a33786a6fe66e7458e4bdf1418149c0
konrad <konradrub14@gmail.com>
2020-11-11 08:45:06 +0100

    QWDF

---
+++ src/TRABAJO_SEMANAL_4_.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+import java.util.Scanner;
+
+/**
+ * @author konrad
+ *
+ */
+public class TRABAJO_SEMANAL_4_ {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int peso_ele = 0;
+		int aguante_tela;
+		
+		Scanner sc= new Scanner(System.in);
+		while(peso_ele !=0) {
+			System.out.println("Cual es el aguante de la telaraña:");
+			aguante_tela=sc.nextInt();
+			
+			System.out.println("¿Cuanto pesa el elefante?");
+			peso_ele=sc.nextInt();
+		}
+		
+
+	}
+
+}
